610e9a94033266dd130adbdef502c8ad52f2b579
Dinokid <90265494+Dinokidtheking@users.noreply.github.com>
2021-09-21 18:38:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var album=["https://thumbs.dreamstime.com/z/cute-little-indian-boy-stands-thinking-cute-little-indian-boy-stands-thinking-holding-his-hand-toy-red-bus-string-197862319.jpg", "https://www.how-to-draw-funny-cartoons.com/images/cartoon-indian-009.jpg","http://clipart-library.com/img/1909838.jpg","https://www.pinclipart.com/picdir/big/30-306310_desi-family-jester-john-indian-father-clipart-png.png"];
+var i=0;
+function nextslide()
+{document.getElementById("pic1").src=album[i];
+ i++;
+}
